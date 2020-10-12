a7d9a88f44fee31aa28d34d1678d65a32659fced
francesco <vignif@gmail.com>
2020-10-12 20:48:12 +0200

    Reorganize

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# parser_sql
-simple parser for sql standard language, project made for Language Processing Technologies @diism University of Siena. feel free to use it for academic purposes
+# SQL parser with LEX / YACC
+Simple parser for sql standard language, project made for Language Processing Technologies @diism University of Siena.
+This project presents a really simple parser for SQL language, with limited use cases and tokens supported.
+It's made for academic purposes and learning purposes, feel free to dig into the details of the **rules** and **definitions** in the source code.
+The compiled version of the program is also provided.
+
+![logo](https://foss.unh.edu/projects/images/c/c8/Lex_Yacc.JPG)
+
+
+An exensive description of the project is presented in: **lex_yacc_project.pdf**
+
+## Statement of the problem
+
+```
+Using lex/yacc implement a parser for the select SQL statement.
+The syntax of the command is
+
+select col1, col2,… from tab1, tab2,.. where cond
+
+where the column (col1, col2,…) and table (tab1, tab2,..) names
+have the usual structure of identifiers (alphanumeric strings
+beginning with a letter) and cond is the condition that
+is written by combining terms
+
+col op col
+col op const
+
+using the connectors and, or. col is a name of a column, const is a constant
+and op is a comparison operator among <, >, =. For instance
+
+select serial, name, salary from employees, salaries where serial = code and
+name = john and salary > 1000
+
+In the code just print the matched terms.
+
+```
+
+# How to use
+There are two steps in compiling a Lex source program. First, the Lex source must be turned into a generated program in the host general purpose language. Then this program must be compiled and loaded, usually with a library of Lex subroutines.
+
+
+
+## Compile the executable in your machine
+
+I've been using a linux distribution for this project, so make sure you have **yacc** and **lex** installed.
+
+```
+sudo apt-get update
+sudo apt-get install bison
+sudo apt-get install flex
+```
+
+
+Now you can build the executable for your own SQL parser with:
+
+```
+yacc -d --verbose sql.y
+lex sql.l
+gcc lex.yy.c y.tab.c utils.c -o sql -ll -ly -w
+```
+The executable is created in **./sql**. Make sure the file can be executed with `chmod +x ./sql`.
+
+The file `text.txt` defines line per line some dumb sql queries to be tested with your brand new parser.
+
+```
+./sql <test.txt
+```
+
+## Use directly the executable
+
+Alternatively you can also try directly the executable without compiling it on your machine.
+
+The file `text.txt` defines line per line some dumb sql queries to be tested with your brand new parser.
+
+```
+./sql <test.txt
+```
+
--- y.tab.h
+++ y.tab.h
@@ -1,14 +1,14 @@
-/* A Bison parser, made by GNU Bison 2.3.  */
+/* A Bison parser, made by GNU Bison 3.5.1.  */
 
-/* Skeleton interface for Bison's Yacc-like parsers in C
+/* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2020 Free Software Foundation,
+   Inc.
 
-   This program is free software; you can redistribute it and/or modify
+   This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,9 +16,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.  */
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -33,21 +31,33 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* Tokens.  */
+/* Undocumented macros, especially those whose name start with YY_,
+   are private implementation details.  Do not rely on them.  */
+
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     NL = 258,
-     IDENTIFIER = 259,
-     CONST = 260,
-     SELECT = 261,
-     FROM = 262,
-     WHERE = 263,
-     AND = 264,
-     OR = 265
-   };
+  enum yytokentype
+  {
+    NL = 258,
+    IDENTIFIER = 259,
+    CONST = 260,
+    SELECT = 261,
+    FROM = 262,
+    WHERE = 263,
+    AND = 264,
+    OR = 265
+  };
 #endif
 /* Tokens.  */
 #define NL 258
@@ -59,25 +69,28 @@
 #define AND 264
 #define OR 265
 
-
-
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
-#line 10 "sql.y"
+union YYSTYPE
 {
+#line 10 "sql.y"
+
 struct{
 	int numerorighe;
 	symbtbl *Mystr;
 };
-}
-/* Line 1529 of yacc.c.  */
-#line 76 "y.tab.h"
-	YYSTYPE;
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
-# define YYSTYPE_IS_DECLARED 1
+
+#line 84 "y.tab.h"
+
+};
+typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
 #endif
+
 
 extern YYSTYPE yylval;
 
+int yyparse (void);
+
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
